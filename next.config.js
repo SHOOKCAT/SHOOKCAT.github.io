f41e06fb468ae6d0ff4d42aa34a83206f5435f95
Mekan Bairyev <bayryewm@gmail.com>
2024-11-30 06:00:30 +0600

    Configure GitHub Pages deployment

--- next.config.js
+++ next.config.js
@@ -4,8 +4,8 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
-  basePath: process.env.NODE_ENV === 'production' ? '/your-repo-name' : '',
-  assetPrefix: process.env.NODE_ENV === 'production' ? '/your-repo-name/' : '',
+  basePath: process.env.NODE_ENV === 'production' ? '/SHOOKCAT.github.io' : '',
+  assetPrefix: process.env.NODE_ENV === 'production' ? '/SHOOKCAT.github.io/' : '',
 }
 
 module.exports = nextConfig 
